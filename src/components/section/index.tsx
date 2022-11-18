25f0e07f822bb9b8d013538286650b68836c6102
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2022-11-18 21:04:48 +0530

    try

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -1,4 +1,6 @@
-export const Section: React.FC = (props) => {
+import { Props } from "@components/types"
+
+export const Section: React.FC = (props:Props) => {
     return(
         <div id={props.id} className="section h-screen pt-16" style={{backgroundColor:props.bgClr}}>
             <div className="text-4xl font-bold text-center">{props.title}</div>
---
+++ src/components/types.tsx
@@ -0,0 +1,4 @@
+export type Props={
+    id:string;
+    title:string;
+}
