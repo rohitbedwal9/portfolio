7135769cf319b21466f4ab9bbc27ba6905d1ee45
Rohit Singh Bedwal <rohitbedwal9@gmail.com>
2023-10-26 01:32:04 +0530

    Added initial files

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menu-items');
+    const navigation = document.querySelector('.hamburger-icon');
+    menuToggle.classList.toggle('open');
+    navigation.classList.toggle('open');
+}
+
+// devlopermode disable
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
